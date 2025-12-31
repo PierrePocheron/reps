7f3330510887e11a47ce99c3b49049e4913c169a
Pierre POCHERON <pierre.pocheron@gmail.com>
2025-12-31 14:59:40 +0100

    fix: update app domain

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.reps.app',
+  appId: 'com.pierre.reps.app',
   appName: 'Reps',
   webDir: 'dist',
   server: {
